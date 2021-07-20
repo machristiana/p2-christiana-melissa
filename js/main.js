7d616b4b3217c9c4fb088073cf9556206d8a24c8
Melissa Christiana <machristiana@ufl.edu>
2021-07-20 08:58:06 -0400

    Style about.html page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-$(".menu").click(function() {
-    $(".menu").toggleClass("active");
+//Change photo based on window size
+var responsive_photo = document.getElementById("responsive");
 
-    $(".navbar-menu").toggleClass("active");
-});
+function swapPhoto() {
+    if (window.innerWidth > 600) {
+        responsive_photo.src = "images/about1.jpeg";
+    } else {
+        responsive_photo.src = "images/about3.jpg";
+    }
+}
+
+swapPhoto();
+
+
+//This makes the swap happen if the user manually resizes the browser after load
+window.addEventListener("resize", swapPhoto, false);
